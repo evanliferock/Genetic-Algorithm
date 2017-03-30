514a4908c2d7719886377c4e65c5a5205dd99814
evanliferock <evanliferock@gmail.com>
2017-03-30 16:32:41 -0700

    Added a starting file

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main{
+  /**
+   * This will call whatever is neccessary to start our program
+   */
+  public static void main(String[] args){
+    System.out.println("Implement me!");
+  }
+}
