d5b3b3a70619cd2bba5609a6c73c5d7ad0e30961
evanliferock <evanliferock@gmail.com>
2017-03-30 19:05:27 -0700

    Added some more methods to be implemented

--- Chromosome.java
+++ Chromosome.java
@@ -4,5 +4,13 @@ public abstract class Chromosome{
     */
    public abstract int cost();
    
+   /**
+    * Creates a new randomized Chromosome.
+    */
    public abstract Chromosome createNew();
+   
+   /**
+    * Mates the calling object and the passed in object
+    */
+   public abstract Chromosome mate(Chromosome b);
 }
