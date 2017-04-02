ceb9ae6f6e40640d52c3d4b8297ff0691f438943
evanliferock <evanliferock@gmail.com>
2017-04-02 15:43:01 -0700

    Created Route constructor

--- Route.java
+++ Route.java
@@ -1,6 +1,11 @@
+import java.util.Collections;
+import java.util.Arrays;
+import java.util.Random;
+
 public abstract class Route extends Chromosome{
    char[] nodes;
    final int[][] matrix;
+   char[] route;
    
    Route(int[][] theMatrix){
       matrix = theMatrix;
@@ -8,5 +13,27 @@ public abstract class Route extends Chromosome{
       for(int i = 0; i < nodes.length; i++){
          nodes[i] = (char)(97 + i);
       }
+      
+      route = nodes.clone();
+      
+      //copied from http://stackoverflow.com/questions/1519736/random-shuffling-of-an-array
+      //because I was too lazy to write it.
+      char temp;
+      int index;
+      Random random = new Random();
+      for (int i = route.length - 1; i > 0; i--){
+        index = random.nextInt(i + 1);
+        temp = route[index];
+        route[index] = route[i];
+        route[i] = temp;
+      }
+      for(char a : route)
+         System.out.println(a);
    }
+   
+   public int cost(){
+      //for(int i = 0;
+      return 0;
+   }
+
 }
