1ade7c0dece8668f4352d3b61b4bb4f7ff04bca9
evanliferock <evanliferock@gmail.com>
2017-04-02 16:09:50 -0700

    Added skeleton classes to be implemented

---
+++ TopDownPopulous.java
@@ -0,0 +1,12 @@
+public class TopDownPopulous extends Populous{
+   TopDownPopulous(Chromosome init, int numValues){
+      super(init, numValues);
+   }
+   /**
+    * Will run the Top-Down algorithm for pairing
+    */
+   protected void nextGeneration(){
+      
+   }
+
+}
---
+++ TournamentPopulous.java
@@ -0,0 +1,11 @@
+public class TournamentPopulous extends Populous{
+   TournamentPopulous(Chromosome init, int numValues){
+      super(init, numValues);
+   }
+   /**
+    * Will run the Tournament algorithm for pairing
+    */
+   protected void nextGeneration(){
+      
+   }
+}
