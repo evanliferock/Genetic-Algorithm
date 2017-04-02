92a9cdf94cd1bfce5253fd0476379e292b0ee01d
evanliferock <evanliferock@gmail.com>
2017-04-02 15:24:09 -0700

    updated Route

--- Route.java
+++ Route.java
@@ -1,3 +1,12 @@
 public abstract class Route extends Chromosome{
+   char[] nodes;
+   final int[][] matrix;
    
+   Route(int[][] theMatrix){
+      matrix = theMatrix;
+      nodes = new char[matrix.length];
+      for(int i = 0; i < nodes.length; i++){
+         nodes[i] = (char)(97 + i);
+      }
+   }
 }
