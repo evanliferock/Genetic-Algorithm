78a912adfff852ca4912b423f187c5135b08e5e5
evanliferock <evanliferock@gmail.com>
2017-04-04 21:01:17 -0700

    implemented the tournament pairing

--- TournamentPopulous.java
+++ TournamentPopulous.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class TournamentPopulous extends Populous{
    TournamentPopulous(Chromosome init, int numValues){
       super(init, numValues);
@@ -6,6 +9,25 @@ public class TournamentPopulous extends Populous{
     * Will run the Tournament algorithm for pairing
     */
    protected void nextGeneration(){
+      ArrayList<Integer> indexesUsed = new ArrayList<Integer>();
+      for(int i = 0; i < chromosomes.length; i++){
+         indexesUsed.add(i);
+      }
+      Random rand = new Random();
       
+      int indexOne, indexTwo;
+      int theSize = indexesUsed.size();
+      Chromosome[] thePair;
+      while(0 < theSize){
+         indexOne = indexesUsed.get(rand.nextInt() % theSize);
+         indexesUsed.remove(indexOne);
+         theSize = indexesUsed.size();
+         indexTwo = indexesUsed.get(rand.nextInt() % theSize);
+         indexesUsed.remove(indexTwo);
+         thePair = chromosomes[indexOne].mate(chromosomes[indexTwo]);
+         theSize = indexesUsed.size();
+         chromosomes[indexOne] = thePair[0];
+         chromosomes[indexTwo] = thePair[1];
+      }
    }
 }
